b6ac5d182e1ebf7c2515be22ae711df2bddab5b7
danizaghian <dani.zaghian@gmail.com>
2019-05-06 11:00:05 -0700

    adds jbuilder view

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,8 +1,15 @@
 class Api::ExamplePagesController < ApplicationController
 
 	def hello_action
+		@message = "Hello"
+		@time = Time.now.strftime("%A, %d %b %Y %l:%M %p")
 		# web response
-		render json: {message: "hello"}
+		render 'hello.json.jbuilder'
+	end
+
+	def goodbye_action
+		# web response
+		render json: {message: "goodbye"}
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/hello_url" => "api/example_pages#hello_action"
+  get "/goodbye_url" => "api/example_pages#goodbye_action"
 end
